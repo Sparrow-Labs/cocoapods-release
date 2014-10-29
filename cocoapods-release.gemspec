f02b1a96fc574169d9599aea6d929bdae3f5f6fd
Oliver Letterer <oliver.letterer@gmail.com>
2014-10-29 20:18:32 +0100

    Implements pod release.

--- cocoapods-release.gemspec
+++ cocoapods-release.gemspec
@@ -8,9 +8,9 @@ Gem::Specification.new do |spec|
   spec.version       = CocoapodRelease::VERSION
   spec.authors       = ["Oliver Letterer"]
   spec.email         = ["oliver.letterer@gmail.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
-  spec.homepage      = ""
+  spec.summary       = %q{CocoaPods release helper.}
+  spec.description   = %q{Release helper plugin for CocoaPods.}
+  spec.homepage      = "https://github.com/Sparrow-Labs/cocoapods-release"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files -z`.split("\x0")
--- lib/pod/command/release.rb
+++ lib/pod/command/release.rb
@@ -1,26 +1,92 @@
 module Pod
   class Command
-    # The pod plugins command.
-    #
     class Release < Command
-      # require 'pod/command/plugins/list'
-      # require 'pod/command/plugins/search'
-      # require 'pod/command/plugins/create'
-
       self.summary = 'Release podspecs in current directory'
-      # self.description = <<-DESC
-      #   Lists or searches the available CocoaPods plugins
-      #   and show if you have them installed or not.
-      #   Also allows you to quickly create a new Cocoapods
-      #   plugin using a provided template.
-      # DESC
+
+      def execute(command)
+        UI.puts "#{"==>".magenta} #{command}"
+        abort unless system(command)
+      end
+
+      self.arguments = [
+        CLAide::Argument.new('repository', false),
+      ]
+
+      def self.options
+        [
+          ['--allow-warnings', 'Allows push even if there are lint warnings'],
+        ].concat(super.reject { |option, _| option == '--silent' })
+      end
+
+      def initialize(argv)
+        warnings = argv.flag?('allow-warnings')
+        @allow_warnings = warnings ? "--allow-warnings" : ""
+        @only_errors = warnings ? "--only-errors" : ""
+
+        @repo = argv.shift_argument unless argv.arguments.empty?
+        super
+      end
 
       def run
-        puts "It worked"
-        # path = get_path_of_spec(@name)
-        # spec = Specification.from_file(path)
-        # UI.puts "Opening #{spec.name} documentation"
-        # `open "http://cocoadocs.org/docsets/#{spec.name}"`
+        specs = Dir.entries(".").select { |s| s.end_with? ".podspec" }
+        abort "No podspec found" unless specs.count > 0
+
+        for spec in specs
+          name = spec.gsub(".podspec", "")
+          version = Specification.from_file(spec).version
+
+          sources = SourcesManager.all
+          sources = sources.select { |s| s.name == @repo } if @repo
+          pushed_sources = []
+
+          abort "Please run #{"pod install".green} to continue" if sources.count == 0
+          for source in sources
+            pushed_versions = source.versions(name)
+            next unless pushed_versions
+
+            pushed_sources << source
+            pushed_versions = pushed_versions.collect { |v| v.to_s }
+            abort "#{name} (#{version}) has already been pushed to #{source.name}".red if pushed_versions.include? version.to_s
+          end
+
+          repo_unspecified = pushed_sources.count == 0 && sources.count > 1
+          if repo_unspecified
+            puts "When pushing a new podspec, please specify a repository to push #{name} to:"
+            puts ""
+            for source in sources
+              puts "  * pod release #{source.name}"
+            end
+            puts ""
+            abort
+          end
+
+          if pushed_sources.count > 1
+            puts "#{name} has already been pushed to #{pushed_sources.join(', ')}. Please specify a repository to push #{name} to:"
+            puts ""
+            for source in sources
+              puts "  * pod release #{source.name}"
+            end
+            puts ""
+            abort
+          end
+
+          # verify lib
+          execute "pod lib lint #{spec} #{@only_errors}"
+
+          # TODO: create git tag for current version
+          unless system("git tag | grep #{version} > /dev/null")
+            execute "git add -A && git commit -m \"Releases #{version}.\""
+            execute "git tag #{version}"
+            execute "git push && git push --tags"
+          end
+
+          repo = pushed_sources.first.name
+          if repo == "master"
+            execute "pod trunk push #{spec} #{@allow_warnings}"
+          else
+            execute "pod repo push #{repo} #{spec} #{@allow_warnings}"
+          end
+        end
       end
     end
   end
