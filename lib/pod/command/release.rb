27b3142a1099ae1868647921fcf9c3103af8ebcc
Oliver Letterer <oliver.letterer@gmail.com>
2015-05-10 20:01:41 +0200

    Adds carthage support

--- lib/pod/command/release.rb
+++ lib/pod/command/release.rb
@@ -3,9 +3,11 @@ module Pod
     class Release < Command
       self.summary = 'Release podspecs in current directory'
 
-      def execute(command)
+      def execute(command, options = {})
+        options = { :optional => false }.merge options
+
         puts "#{"==>".magenta} #{command}"
-        abort unless system(command)
+        abort unless (system(command) || options[:optional])
       end
 
       self.arguments = [
@@ -15,6 +17,7 @@ module Pod
       def self.options
         [
           ['--allow-warnings', 'Allows push even if there are lint warnings'],
+          ['--carthage', 'Validates project for carthage deployment'],
         ].concat(super.reject { |option, _| option == '--silent' })
       end
 
@@ -22,6 +25,7 @@ module Pod
         warnings = argv.flag?('allow-warnings')
         @allow_warnings = warnings ? "--allow-warnings" : ""
         @repo = argv.shift_argument unless argv.arguments.empty?
+        @carthage = argv.flag?('carthage')
         super
       end
 
@@ -35,6 +39,7 @@ module Pod
         for spec in specs
           name = spec.gsub(".podspec", "")
           version = Specification.from_file(spec).version
+          name = Specification.from_file(spec).name
 
           sources = SourcesManager.all.select { |r| r.name == "master" || r.url.start_with?("git") }
           sources = sources.select { |s| s.name == @repo } if @repo
@@ -76,9 +81,13 @@ module Pod
           execute "pod lib lint #{spec} #{@allow_warnings} --sources=#{available_sources.join(',')}"
           execute "pod lib lint #{spec} --use-libraries #{@allow_warnings} --sources=#{available_sources.join(',')}"
 
+          if @carthage
+            execute "carthage build --no-skip-current"
+          end
+
           # TODO: create git tag for current version
           unless system("git tag | grep #{version} > /dev/null")
-            execute "git add -A && git commit -m \"Releases #{version}.\""
+            execute "git add -A && git commit -m \"Releases #{version}.\"", :optional => true
             execute "git tag #{version}"
             execute "git push && git push --tags"
           end
@@ -88,6 +97,25 @@ module Pod
             execute "pod trunk push #{spec} #{@allow_warnings}"
           else
             execute "pod repo push #{repo} #{spec} #{@allow_warnings}"
+          end
+
+          if @carthage && `git remote show origin`.include?("git@github.com")
+            execute "carthage archive #{name}"
+
+            user, repo = /git@github.com:(.*)\/(.*).git/.match(`git remote show origin`)[1, 2]
+            file = "#{name}.framework.zip"
+
+            create_release = %(github-release release --user #{user} --repo #{repo} --tag #{version} --name "Version #{version}" --description "Release of version #{version}")
+            upload_release = %(github-release upload --user #{user} --repo #{repo} --tag #{version} --name "#{file}" --file "#{file}")
+
+            if ENV['GITHUB_TOKEN'] && system("which github-release")
+              execute create_release
+              execute upload_release
+              execute "rm #{file}"
+            else
+              puts "Run `#{create_release} --security-token XXX` to create a github release and"
+              puts "    `#{upload_release} --security-token XXX` to upload to github releases"
+            end
           end
         end
       end
